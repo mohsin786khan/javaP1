14b095d4b3db1c5451813eb77ca20680896fb84f
mohsin khan <khan.mohsin.00001@gmail.com>
2022-10-26 00:05:12 +0530

    practice project

---
+++ src/DeskPhone.java
@@ -0,0 +1,50 @@
+public class DeskPhone implements Telephone{
+
+    private String myNumber;
+    private boolean isRinging;
+
+    public DeskPhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.isRinging=false;
+    }
+
+    @Override
+    public void powerOn() {
+         System.out.println("Desk phone is always on");
+    }
+
+    @Override
+    public void dial(String phoneNo) {
+           System.out.println("Dialing the phoneNo "+phoneNo);
+    }
+
+    @Override
+    public void answer() {
+        if(isRinging()==true){
+            System.out.println("answer the phone");
+            isRinging=false;
+        }else{
+
+        }
+    }
+
+    @Override
+    public boolean receivePhone(String phoneNo) {
+
+        if(phoneNo==myNumber){
+            isRinging=true;
+            System.out.println("phone is ringing");
+        }else{
+            isRinging=false;
+            System.out.println("wrong number");
+        }
+
+        return  isRinging;
+    }
+
+    @Override
+    public boolean isRinging() {
+
+        return isRinging;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+           DeskPhone d1 = new DeskPhone("1234567");
+
+           d1.powerOn();
+
+           boolean callaarahihai = d1.receivePhone("1234567");
+
+           if(callaarahihai==true){
+               System.out.println("call aarahi hai");
+           }else{
+               System.out.println("call nhi aarahi hai");
+           }
+    }
+}
